9d6a5cc01fd701ed5cbe4fb46cf68f750ffebab3
Paulo Silva <pauloalexandresilva7689@gmail.com>
2020-12-29 23:36:50 +0100

    New

--- guppe/pep8.py
+++ guppe/pep8.py
@@ -21,6 +21,22 @@ A ideia da PEP8 é que possamos escrever códigos Python de forma Pythónica.
 
     def soma_dois():
         pass
+
+[3] - Utilize 4 espaços para identação!(muito importante);
+    if 'a' in 'banana':
+    ____print('tem') "(não é recomendado usar o tab, pois em alguns pc's
+    pode ser diferente de 4 espaços) 4 espaços se não resulta em erro
+    de identeação"
+
+[4] - Linhas em branco;
+        - Para definir uma classe é preciso 2 linhas em branco;
+        - Separar funções e definições de classe com duas linhas em branco;
+        - Métodos dentro de uma classe devem ser separados com uma única
+        linha em branco.
+
+[5] - Imports:
+        - devem ser sempre feitos em linhas separadas;
+        -
 """
 
 
@@ -39,3 +55,5 @@ def soma():
 def soma_dois():
     pass
 
+if 'a' in 'banana':
+    print('tem')
