74b327c1494e935552894707f25f8968ef38c9e2
Paulo Silva <pauloalexandresilva7689@gmail.com>
2020-12-29 23:24:46 +0100

    New

---
+++ guppe/pep8.py
@@ -0,0 +1,41 @@
+"""
+PEP8 - Python Enhancement Proposal
+
+São propostas de melhorias para a linguagem Python
+
+The Zen of Python
+
+import this
+
+A ideia da PEP8 é que possamos escrever códigos Python de forma Pythónica.
+
+[1] - Utilize Camel Case para os nomes de classes;
+    EX: class Calculadora:
+
+        class CalculadoraCientifica:
+
+[2] - Utilize nomes em minusculo, separados por underline funções ou variáveis;
+    Ex: def soma():
+        pass
+
+
+    def soma_dois():
+        pass
+"""
+
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+def soma():
+    pass
+
+
+def soma_dois():
+    pass
+
