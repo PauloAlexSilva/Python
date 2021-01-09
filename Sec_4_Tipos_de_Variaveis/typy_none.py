0aacc7fadeae4d7d5498493410b85a250ff7b083
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-09 15:35:27 +0100

    None

---
+++ Sec_4_Tipos_de_Variaveis/typy_none.py
@@ -0,0 +1,29 @@
+"""
+Tipo None
+
+O tipo de dado Nome em Python representa o tipo sem tipo, ou poderia ser conhecido
+também como tipo vazio, porém, falar que é um tipo sem tipo é mais apropriado.
+
+numeros = None
+print(numeros)
+print(type(numeros))
+
+None
+<class 'NoneType'>
+
+OBS: O tipo None é SEMPRE especificado com a primeira letra maiúscula
+
+Quando utilizar?
+    - Podemos utilizar None quando queremos criar uma variável e inicializá-la com
+    um tipo sem tipo, antes de recebermos um valor final.
+
+"""
+
+numeros = None
+print(numeros)
+print(type(numeros))
+
+numeros = 1.44, 1.34, 5.67
+
+print(numeros)
+print(type(numeros))
