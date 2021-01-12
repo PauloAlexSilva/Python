c46bf4f2e934ebf6e3a2be3a6babb26a267d48ad
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-12 01:20:19 +0100

    Sets

---
+++ Sec_7_colecoes/conjuntos.py
@@ -0,0 +1,42 @@
+"""
+Conjuntos
+
+- Conjuntos em qualquer linguagem de programação, estamos fazendo referência à
+Teoria dos Conjuntos da Matemática.
+
+- Em Python, os conjuntos são chamados de Sets.
+
+Dito isto, da mesma forma que na matemática:
+    - Sets (conjuntos) não possuem valores duplicados;
+    - Sets (conjuntos) não possuem valores ordenados;
+    - Elementos não são acessados via índice, ou seja, conjuntos não são
+    indexados.
+
+Conjuntos são bons para se utilizar quando precisameos armazenar elementos
+mas não nos importamos com a ordenação deles.
+Quando não precisamos de nos preocupar com chaves, valores e itens duplicados
+
+Os conjuntos (sets) são referênciados em Python com chaves {}
+
+Diferença entre Conjuntos (Sets) e Mapas(Dicionários) em Python:
+    - Um dicionário tem chave/valor;
+    - Um conjunto tem apenas valor;
+
+"""
+
+# Definindo um conjunto
+
+# Forma 1
+
+s = set({1, 2, 3, 4, 5, 5, 6, 7, 2, 3})  # Temos valores repetidos
+print(s)
+print(type(s))
+
+# OBS: Ao criar um conjunto, caso seja adicionado um valor já existente, o
+# mesmo será ignorado sem gerar erro e não fará parte do conjunto
+
+# Forma 2 - Mais comum
+
+s = {1, 2, 3, 4, 5, 5}
+print(s)
+print(type(s))
