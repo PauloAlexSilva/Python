f3ba7e4d878b1a66171489772e14857ba599993d
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-25 23:30:50 +0100

    Ordered Dict

---
+++ Sec_7_colecoes/ordered_dict.py
@@ -0,0 +1,14 @@
+"""
+Módulo Collecns: Ordered Dict
+
+
+
+"""
+
+# Num dicionário a ordem de inserção dos elementos não é garantida
+dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+for chave, valor in dicionario.items():
+    print(f'Chave={chave}:valor={valor}')
+
+print(dicionario)
