683134dd56cee2fe4a854562a512e57e4364bc50
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-28 01:06:06 +0100

    Named Tuple

--- Sec_7_colecoes/named_tuple.py
+++ Sec_7_colecoes/named_tuple.py
@@ -6,6 +6,49 @@ tupla = (1, 2, 3)
 
 print(tupla[1])
 
-Named Tuple ->
+Named Tuple -> São tuplas, deferenciadas, onde, especificamos um nome para a mesma
+e também parâmetros.
+
+
 """
 
+# Import
+from collections import namedtuple
+
+# Precisamos definir o nome e parâmetros
+
+# Forma 1 - Declaração Named Tuple
+
+cao = namedtuple('cao', 'idade raca nome')
+
+# Forma 2 - Declaração Named Tuple
+
+cao_2 = namedtuple('cao', 'idade, raca, nome')
+
+# Forma 3 - Declaração Named Tuple
+
+cao_3 = namedtuple('cao', ['idade', 'raca', 'nome'])
+
+# Usar
+
+ray = cao(idade=2, raca='Labrador', nome='Stors')
+
+print(ray)
+
+# Acessar o dados
+
+# Forma 1
+
+print(ray[0])  # idade
+print(ray[1])  # nome
+print(ray[2])  # raca
+
+# Fomra 2
+
+print(ray.idade)  # idade
+print(ray.nome)  # nome
+print(ray.raca)  # raca
+
+print(ray.index('Stors'))
+
+print(ray.count('Stors'))
