c30e509ea9a10a48c16dad744b68f8265ec5d64f
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-29 22:24:24 +0100

    Funções com Retorno

---
+++ Sec_8_funcoes/funcoes_com_retorno.py
@@ -0,0 +1,26 @@
+"""
+Funções com Retorno
+
+numeros = [1, 2, 3]
+
+ret_pop = numeros.pop()
+
+print(f'Retorno de pop: {ret_pop}')
+
+ret_pr = print(numeros)
+
+print(f'Retorno de print: {ret_pr}')
+
+
+"""
+
+
+# Exemplo função
+
+def quadrado_de_7():
+    print(7 * 7)
+
+
+ret = quadrado_de_7()
+
+print(f'Retorno {ret}')
