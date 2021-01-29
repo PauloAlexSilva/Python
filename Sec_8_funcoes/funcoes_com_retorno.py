b79860a6f514051bcbf9c2d813a2e87141e983a7
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-29 23:25:55 +0100

    Funções com Retorno

--- Sec_8_funcoes/funcoes_com_retorno.py
+++ Sec_8_funcoes/funcoes_com_retorno.py
@@ -11,16 +11,81 @@ ret_pr = print(numeros)
 
 print(f'Retorno de print: {ret_pr}')
 
+OBS: Em Python quando uma função não retorna nenhum valor, o retorno é None
+
+OBS: Funções Python que retornam valores devem retornar estes valores com a palavra
+reservada return.
+
+OBS: Não é preciso necessariamente criar uma variável para receber o retorno de uma função.
+Podemos passar a execução da função para outras funções.
+
+# Vamos refatorar esta função para que ela retorne o valor
+
+def quadrado_de_7():
+    return 7 * 7
+
+
+# Criamos uma variável para receber o retorno da função
+
+ret = quadrado_de_7()
+print(f'Retorno: {ret}')
+print(f'Retorno: {quadrado_de_7() + 1}')
+
+
+# Refatorando a primeira função
+
+def diz_ola():
+    return 'Hello '
+
+
+alguem = 'Pedro!'
+
+print(diz_ola() + alguem)
+
+OBS: Sobre a palavra reservada retrun
+
+    1 -  Finaliza a função, ou seja sai da execução da função;
+    2 - Podemos ter, em uma função diferentes returns;
+    3 - Podemos, numa função retornar qualquer tipo de dado e até mesmo múltiplos valores;
+
+
+# Exemplo 1
+
+    def hello():
+        print('Antes do return.')
+        return 'Hello!'
+        print('Apos return.') # Nunca é executado
+
+
+    print(hello())
+
+# Exemplo 2
+
+    def nova_funcao():
+        variavel = False
+        if variavel:
+            return 4
+        elif variavel is None:
+            return 3.2
+        return 'b'
+
+
+    print(nova_funcao())
+
+# Exemplo 3
+
+    def outra_funcao():
+        return 2, 3, 4, 5
+
+
+    # num1, num2, num3, num4 = outra_funcao()
+    # print(num1, num2, num3, num4)
+
+    print(outra_funcao())
+    print(type(outra_funcao()))
+
 
 """
 
 
-# Exemplo função
 
-def quadrado_de_7():
-    print(7 * 7)
-
-
-ret = quadrado_de_7()
-
-print(f'Retorno {ret}')
