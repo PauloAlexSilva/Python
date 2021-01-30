d2df108f02eef059cf8a579ade512f576fc6effa
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-01-30 16:57:45 +0100

    Default Paramters

---
+++ Sec_8_funcoes/funcoes_com_parametro_padrao.py
@@ -0,0 +1,76 @@
+"""
+Funções com Parâmetro Padrão (Default Paramters)
+
+- Funções onde a passagem de parâmetro seja opcional;
+
+# Exemplo de função onde a passagem de parâmetro é opcional
+
+print('Hello World!')
+
+print()
+
+
+# Exemplo de função onde a passagem de parâmetro seja obrigatória
+
+def quadrado(numero):
+    return numero ** 2
+
+
+print(quadrado(3))
+print(quadrado()) # TypeError
+
+
+def exponencial(numero, potencia=2):  # Passar um valor por parâmtro ele se torna opcional
+    return numero ** potencia
+
+
+print(exponencial(2, 3))  # 2 * 2 * 2 = 8
+print(exponencial(3, 2))  # 3 * 3 = 9
+
+print(exponencial(3))  # Por padrão elevado ao quadrado
+print(exponencial(3, 5))  # Elevado à potência informada pelo utilizador
+
+# Se o utilizador passar apenas um argumento, este será atribuido ao parâmetro "numero",
+# e será calculado o quadrado deste número;
+# Se o utilizador passar dois argumentos, o primeiro será atribuído ao parâmetro "numero" e
+# o segundo ao parâmetro "potencia". Então será calculada esta potência.
+
+
+# OBS : Em funções Python, os parâmetros com valores default (padrão) DEVEM sempre
+# estar no final da declaração
+
+# ERRO!
+def teste(num=2, potencia):
+    return num ** potencia
+
+
+print(teste(6))
+
+
+# Outros exemplos
+
+def soma(num1, num2):
+    return num1 + num2
+
+
+print(soma(4, 3))
+print(soma(4))  # TypeError
+print(soma())  # TypeError
+
+
+"""
+
+
+# Exemplo mais complexo
+
+def mostra_informacao(nome='Paulo', instrutor=False):
+    if nome == 'Paulo' and instrutor:
+        return 'Bem vindo instrutor Paulo'
+    elif nome == 'Paulo':
+        return 'Eu pensei que era o intrutor!'
+    return f'Olá {nome}'
+
+
+print(mostra_informacao())
+print(mostra_informacao(instrutor=True))
+print(mostra_informacao('Carlos'))
