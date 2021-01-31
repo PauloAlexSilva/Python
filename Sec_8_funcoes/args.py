6a7da89e7921f0e766d609b679becaa5c0c9c652
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-01 00:54:41 +0100

    *args

---
+++ Sec_8_funcoes/args.py
@@ -0,0 +1,17 @@
+"""
+Entendendo o *args
+
+- O *args é um parâmetro, como outro qualquer. Isso significa que poderá chamar de qualquer
+coisa, desde que começe com asterisco.
+
+Exemplo:
+    *assim
+
+Mas por conveção, utilizamos *args para defini-lo.
+
+Mas o que é o *args?
+
+O parâmetro *args utilizado em uma função, coloca os valores extras informados como entrada
+em uma tupla. Não esquecer que as tuplas são imutá
+
+"""
