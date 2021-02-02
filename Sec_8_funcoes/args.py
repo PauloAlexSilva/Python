fc3475dad0d3f645f26250515bdff387a4eb76c5
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-02 22:57:57 +0100

    *args

--- Sec_8_funcoes/args.py
+++ Sec_8_funcoes/args.py
@@ -26,15 +26,14 @@ print(soma(1, 2, 3))
 # print(soma(1, 2))  # TypeError
 # print(soma(1, 2, 3, 4))  # TypeError
 
-"""
 
 # Etendendo o args
 
-"""def soma(*args):
+def soma(*args):
     total = 0
     for numero in args:
         total = total + numero
-    return total"""
+    return total
 
 
 def soma(*args):
@@ -47,3 +46,37 @@ print(soma(1, 2, 3))
 print(soma(1, 2, 3, 4))
 
 print(soma(23.4, 12, 5))
+
+
+# Outro exemplo de utilização do *args
+
+def verifica_info(*args):
+    if 'Paulo' in args and 'Silva' in args:
+        return 'Bem vindo!'
+    return 'Não sei quem é!'
+
+
+print(verifica_info())
+print(verifica_info(1, True, 'Silva', 'Paulo'))
+print(verifica_info(1, 'Silva', 3.145))
+
+
+"""
+
+
+def soma(*args):  # args nome do atributo
+    print(args)
+    return sum(args)
+
+
+# print(soma())
+# print(soma(3, 4, 5, 6))
+
+numeros = [1, 2, 3, 4, 5, 6, 7]
+
+# Desempacotador
+
+print(soma(*numeros))  # colocar '*' no nome do argumento
+
+# OBS: O asterisco serve para que informemos ao Python que estamos passando como argumento
+# uma coleção de dados. Desta forma ele sabe que precisará de desempacotar os dados.
