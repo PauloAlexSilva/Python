b167feb86b3a928fe717aaef70ce16868f4596ea
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-05 17:27:41 +0100

    Dictionary Comprehension

--- Sec_9_comprehensions/dictionary_comprehension.py
+++ Sec_9_comprehensions/dictionary_comprehension.py
@@ -18,7 +18,6 @@ Se quisermos criar um dicionário:
 
 {chave: valor for valor in interável}
 
-"""
 
 # Exemplos
 
@@ -27,3 +26,31 @@ numeros = {'a': 1, 'b': 2, 'c': 3, 'd': 3, 'e': 5}
 quadrado = {chave: valor ** 2 for chave, valor in numeros.items()}
 
 print(quadrado)
+# {'a': 1, 'b': 4, 'c': 9, 'd': 9, 'e': 25}
+
+
+numeros = [1, 1, 2, 3, 4, 5, 5]
+
+quadrado = {valor: valor ** 2 for valor in numeros}
+
+print(quadrado)
+# {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
+
+
+chaves = 'abcde'
+valores = [1, 2, 3, 4, 5]
+
+mistura = {chaves[i]: valores[i] for i in range(0, len(chaves))}
+
+print(mistura)
+# {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+
+"""
+
+# Exemplo com lógica condicional
+
+numeros = [1, 2, 3, 4, 5]
+
+res = {num: ('Par' if num % 2 == 0 else 'Impar') for num in numeros}
+print(res)
