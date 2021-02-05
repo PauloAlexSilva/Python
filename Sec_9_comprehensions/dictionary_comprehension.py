8751bf0cbb667b6b91f8ddf30daee72b99e26056
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-05 17:17:18 +0100

    Dictionary Comprehension

--- Sec_9_comprehensions/dictionary_comprehension.py
+++ Sec_9_comprehensions/dictionary_comprehension.py
@@ -1,6 +1,29 @@
 """
 Dictionary Comprehension
 
+Se quiseremos criar uma lista fazemos:
+    lista = [1, 2, 3, 4]
+
+Se quiseremos criar uma tupla:
+    tupla = (1, 2, 3, 4) # 1, 2, 3, 4
+
+Se quisermos criar um set (conjunto):
+    conjunto = {1, 2, 3, 4}
+
+Se quisermos criar um dicionário:
+    dicionario = {'a': 1, 'b': 2, 'c': 3, 'd': 3}
+
+
+# Sintaxe
+
+{chave: valor for valor in interável}
 
 """
 
+# Exemplos
+
+numeros = {'a': 1, 'b': 2, 'c': 3, 'd': 3, 'e': 5}
+
+quadrado = {chave: valor ** 2 for chave, valor in numeros.items()}
+
+print(quadrado)
