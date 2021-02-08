c93c1b0346e53f3ac2a9d75473e800962ab2603a
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-08 23:23:33 +0100

    Reduce

---
+++ Sec_10_expr_lambdas_fun_integradas/reduce.py
@@ -0,0 +1,16 @@
+"""
+Reduce
+
+OBS: A partir do Python 3+ a função reduce() não é mais uma função integrada (built-in).
+Agora temos que importar e utilizar esta função a partir do módulo 'functools'
+
+Guido van Rossum: Utilize a função reduce() se você realmente precisa dela. Em todo o caso,
+99% das vezes um loop for é mais legível.
+
+Para perceber o reduce()
+
+# Imagine que tem uma coleção de dados:
+
+dados = [a1,
+
+"""
