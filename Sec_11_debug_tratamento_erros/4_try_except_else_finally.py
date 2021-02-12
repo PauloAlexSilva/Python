7d950ea2731fc5a9c0e56ef77b6ae0d8e554de85
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-12 16:46:31 +0100

    Try/Except/Else/Finally

---
+++ Sec_11_debug_tratamento_erros/4_try_except_else_finally.py
@@ -0,0 +1,26 @@
+"""
+Try / Except / Else / Finally
+
+Dica de quando e onde tratar código:
+
+TODA A ENTRADA DEVE SER TRATADA!
+
+OBS: A função do utilizador é DESTRUIR o sistema
+
+# Else -> É executado somente se não ocorrer o erro.
+
+try:
+    num = int(input('Digite um número: '))
+except ValueError:
+    print('Valor incorreto!')
+else:
+    print(f'O número introduzido foi: {num}')
+
+
+"""
+
+# Finally
+
+try:
+    num = int(input())
+
