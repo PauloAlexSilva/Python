541bc9853f19b10394b11fefb7f501c37d347700
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-13 19:00:44 +0100

    Try/Except/Else/Finally

--- Sec_11_debug_tratamento_erros/4_try_except_else_finally.py
+++ Sec_11_debug_tratamento_erros/4_try_except_else_finally.py
@@ -17,12 +17,32 @@ else:
     print(f'O número introduzido foi: {num}')
 
 
-"""
-
 # Finally
 
 try:
     num = int(input('Digite um número: '))
 except ValueError:
     print('Valor inválido!')
-print('Depois do bloco try/except')
+else:
+    print(f'O número introduzido foi: ')
+finally:
+    print('Executando o finally')
+
+# OBS: O bloco finally é sempre executado, indepedentemente se houve ou não execução
+
+# O finally, geralmente, é utilizado para fechar ou desalocar recursos.
+
+
+"""
+
+
+# Exemplo mais complexo
+
+def dividr(a, b):
+    return a / b
+
+
+num1 = int(input('Informe o primeiro numero: '))
+num2 = int(input('Informe o segundo numero: '))
+
+print(dividr(num1, num2))
