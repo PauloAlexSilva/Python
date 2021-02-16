c5a8cb572d17cba6b4bf9221e48f20eddbed1609
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-16 12:43:33 +0100

    New update

---
+++ Sec_12_modulos_pacotes/c_modulos_customizados.py
@@ -0,0 +1,12 @@
+"""
+Módulos Customizados
+
+Como módulos Python são arquivos Python, então TODOS os arquivos que foram criados são módulos Python
+prontos para serem utilizados.
+
+
+"""
+
+from Sec_8_funcoes.c_funcoes_com_parametro import soma_impares
+
+print(soma_impares([1, 2, 3, 4, 5, 6, 7, 8, 9]))
