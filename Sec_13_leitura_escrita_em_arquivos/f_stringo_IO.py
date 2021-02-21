5f3c065b7486d0bfd633ec4ca8600f1344badc1c
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-21 20:00:16 +0100

    StringIO

--- Sec_13_leitura_escrita_em_arquivos/f_stringo_IO.py
+++ Sec_13_leitura_escrita_em_arquivos/f_stringo_IO.py
@@ -1,5 +1,33 @@
 """
-String IO
+StringIO
 
-asas
+ATENÇÃO: Para ler ou escrever dados nos arquivos do Sistema Operacional o software precisa ter
+permissão:
+    - Permissão de Leitura -> Para ler o arquivo;
+    - Permissão de Escrita -> Para escrever.
+
+StringIO -> Utilizado para ler e criar arquivos em memória.
+
 """
+# Primeiro fazemos o import
+
+from io import StringIO
+
+mensagem = 'Esta é uma String normal.'
+
+# Podemos criar um arquivo em memória já com uma String enserida ou mesmo vazia para enserir texto
+# depois.
+
+arquivo = StringIO(mensagem)
+# arquivo = open('arquivo.txt', 'w')
+
+# Agora tendo o arquivo podemos utilizar tudo o que já sabemos
+
+print(arquivo.read())
+
+arquivo.write(' Outro texto!')
+
+# Podemos inclusive movimentar o cursor
+arquivo.seek(0)
+
+print(arquivo.read())
