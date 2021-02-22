79afc5bdc5e3572ec3366c475172352d95c738b5
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-22 22:56:03 +0100

    Sistemas de Arquivos e Navegação

--- Sec_13_leitura_escrita_em_arquivos/g_sistemas_arquivos_navegacao.py
+++ Sec_13_leitura_escrita_em_arquivos/g_sistemas_arquivos_navegacao.py
@@ -1,3 +1,21 @@
 """
-Sistemas de Arquivos - Navegação
+Sistemas de Arquivos e Navegação
+
+Para fazer uso de manipulação de arquivos do sistema operacional, precisamos importar
+e fazer uso do módulo os.
+
+
 """
+
+# Fazer o import
+import os
+
+# getcwd() -> pega o current work directory - diretório de trabalho corrente
+# Retorna o path (caminho absoluto)
+print(os.getcwd())
+
+# Para mudar o diretório, podemos utilizar o chdir()
+
+os.chdir('..')
+
+print(os.getcwd())
