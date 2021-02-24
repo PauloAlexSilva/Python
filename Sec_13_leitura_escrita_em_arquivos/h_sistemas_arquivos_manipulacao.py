23dc41bfce7926bb4706a879dbcb0008d54501dd
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-24 23:50:27 +0100

    Sistema de Arquivos - Manipulação

--- Sec_13_leitura_escrita_em_arquivos/h_sistemas_arquivos_manipulacao.py
+++ Sec_13_leitura_escrita_em_arquivos/h_sistemas_arquivos_manipulacao.py
@@ -6,4 +6,15 @@ Sistema de Arquivos - Manipulação
 import os
 
 # Descobrir se um arquivo ou diretório existe
-print(os.path.exists('arquivo.txt'))
+print(os.path.exists('arquivo.txt'))  # False
+print(os.path.exists('frutas.txt'))  # True
+
+# Diretório
+
+# Paths relativos
+print(os.path.exists('inicio'))  # False
+print(os.path.exists('Python/inicio'))  # False
+
+# Path absolutos
+print(os.path.exists('/home/ubuntu/Python'))  # False
+
