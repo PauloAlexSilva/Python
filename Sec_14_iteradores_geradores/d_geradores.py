1d170a7e4d1f8f735951efc883d247ff520bb2e5
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-02-28 21:48:42 +0100

    Geradores

--- Sec_14_iteradores_geradores/d_geradores.py
+++ Sec_14_iteradores_geradores/d_geradores.py
@@ -1,5 +1,30 @@
 """
 Geradores
 
+- Geradores (Genarators) são Iterators (Iteradores):
+
+OBS: O contrário não é verdadeiro. Ou seja, nem todo iterator é um generator.
+
+Outras informações:
+    - Generators podem ser criados com funções geradoras;
+    - Funções geradoras utilizam a palavra reservada "yield";
+    - Generators podem ser criados com Expressões Geradoras.
+
+Diferenças entre Funções e Generator Funtions (Funções Geradoras)
+
+______________________________________________________________________________
+| Funções                             |     Genatator Functions              |
+------------------------------------------------------------------------------
+| Utilizam return                     | Utilizam yield                       |
+______________________________________________________________________________
+| retorna uma vez                     | podem utilizar yield multiplas vezes |
+------------------------------------------------------------------------------
+| quando executada, retorna um valor  | quando executada retorna um generator|
+------------------------------------------------------------------------------
+
 
 """
+
+# Exemplo de Generator Function
+
+
