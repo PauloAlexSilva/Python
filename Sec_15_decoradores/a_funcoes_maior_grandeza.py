220854321647f772368da7df9a284a52f705d16a
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-03 22:48:39 +0100

    Funçoes de Maior Grandeza

--- Sec_15_decoradores/a_funcoes_maior_grandeza.py
+++ Sec_15_decoradores/a_funcoes_maior_grandeza.py
@@ -1,5 +1,8 @@
 """
-Funçoes de Maior Grandeza
+Funçoes de Maior Grandeza - Higher Order Functions - HOF
 
+- Quando uma linguagem de programação suporta HOF, indica que podemos ter funções que retornam
+outras funções como resultado ou mesmo que podemos passar funções como argumentos para outras funções,
+e até mesmo criar variáveis do tipo de funções nos nossos porgramas.
 
 """
