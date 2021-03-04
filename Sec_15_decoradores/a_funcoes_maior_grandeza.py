c83f51de1b3a1d53a39f9614a857b7bb818ce065
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-04 21:01:21 +0100

    Funçoes de Maior Grandeza

--- Sec_15_decoradores/a_funcoes_maior_grandeza.py
+++ Sec_15_decoradores/a_funcoes_maior_grandeza.py
@@ -5,4 +5,41 @@ Funçoes de Maior Grandeza - Higher Order Functions - HOF
 outras funções como resultado ou mesmo que podemos passar funções como argumentos para outras funções,
 e até mesmo criar variáveis do tipo de funções nos nossos porgramas.
 
+OBS: Na secção de funções foi usado isto.
+
+
+
 """
+
+
+# Exemplos - Definindo as funções
+
+def somar(a, b):
+    return a + b
+
+
+def subtracao(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+
+def dividir(a, b):
+    return a / b
+
+
+def calcular(num1, num2, funcao):
+    return funcao(num1, num2)
+
+
+# Teste das funções
+
+print(calcular(4, 3, somar))
+
+print(calcular(4, 3, subtracao))
+
+print(calcular(4, 3, multiplicar))
+
+print(calcular(4, 3, dividir))
