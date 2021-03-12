cfd7b32cf0c46f5070666a8322aa080ab7933b7e
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-12 22:52:00 +0100

    Funçoes de Maior Grandeza

--- Sec_15_decoradores/c_decoradores_dif_assinaturas.py
+++ Sec_15_decoradores/c_decoradores_dif_assinaturas.py
@@ -1,8 +1,7 @@
 """
 Decoradores com Diferentes Assinaturas
 
-"""
-
+# Para resolver utilizamos um padrão de projeto chamado Decorator Pattern
 
 # Relembrar
 
@@ -11,3 +10,27 @@ def gritar(funcao):
         return funcao(nome).upper()
 
     return aumentar
+
+
+@gritar
+def saudacao(nome):
+    return f'Olá eu sou o {nome}.'
+
+
+@gritar
+def ordenar(principal, acompanhamento):
+    return f'Olá, eu gostaria de {principal}. acompanhamento de {acompanhamento}, por favor.'
+
+
+# Teste
+
+# print(saudacao('Paulo'))
+print(ordenar('Picanha', 'Batata frita'))
+
+
+"""
+
+# Refatorando com a Decorator Pattern
+
+def gritar
+
