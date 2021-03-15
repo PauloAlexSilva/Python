ea68b529a4aed85113e5ad1491f15683db0fd137
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-15 22:33:01 +0100

    Funçoes de Maior Grandeza

--- Sec_15_decoradores/c_decoradores_dif_assinaturas.py
+++ Sec_15_decoradores/c_decoradores_dif_assinaturas.py
@@ -3,6 +3,8 @@ Decoradores com Diferentes Assinaturas
 
 # Para resolver utilizamos um padrão de projeto chamado Decorator Pattern
 
+A assinatura de uma função é representada pelo seu retorno, nome e parâmetros de entrada.
+
 # Relembrar
 
 def gritar(funcao):
@@ -28,9 +30,42 @@ def ordenar(principal, acompanhamento):
 print(ordenar('Picanha', 'Batata frita'))
 
 
-"""
-
 # Refatorando com a Decorator Pattern
 
-def gritar
+def gritar(funcao):
+    def aumentar(*args, **kwargs):
+        return funcao(*args, **kwargs).upper()
+
+    return aumentar
+
+
+@gritar
+def saudacao(nome):
+    return f'Olá eu sou o {nome}.'
+
+
+@gritar
+def ordenar(principal, acompanhamento):
+    return f'Olá, eu gostaria de {principal} acompanhado de {acompanhamento}, por favor.'
+
+
+print(saudacao('Paulo'))
+
+print(ordenar('Picanha', 'Batata Frita'))
+
+
+@gritar
+def lol():
+    return 'lol'
+
+
+print(lol())
+
+# OBS: Podemos utilizar parâmetros nomeados
+
+print(ordenar(acompanhamento='Batata Frita', principal='Picanha'))
+
+"""
+
+# Decorator
 
