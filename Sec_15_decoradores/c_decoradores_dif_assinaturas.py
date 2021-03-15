9d0250e5ce06547ea532178801b38acf6b9d2110
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-15 22:42:23 +0100

    Decoradores com Diferentes Assinaturas

--- Sec_15_decoradores/c_decoradores_dif_assinaturas.py
+++ Sec_15_decoradores/c_decoradores_dif_assinaturas.py
@@ -67,5 +67,36 @@ print(ordenar(acompanhamento='Batata Frita', principal='Picanha'))
 
 """
 
-# Decorator
+
+# Decorator com argumentos
+
+def verifica_argumentos(valor):
+    def interna(funcao):
+        def outra(*args, **kwargs):
+            if args and args[0] != valor:
+                return f'Valor incorreto! Pimeiro argumento precisa de ser {valor}.'
+            return funcao(*args, **kwargs)
+
+        return outra
+
+    return interna
+
+
+@verifica_argumentos('pizza')
+def comida_favorita(*args):
+    print(args)
+
+
+@verifica_argumentos(10)
+def soma_dez(num1, num2):
+    return num1 + num2
+
+
+# Teste
+
+print(soma_dez(10, 12))  # 22
+print(soma_dez(2, 10))  # Valor incorreto! Pimeiro argumento precisa de ser 10.
+
+print(comida_favorita('pizza', 'churrasco'))
+print(comida_favorita('Carne', 'churrasco'))  # Valor incorreto! Pimeiro argumento precisa de ser pizza.
 
