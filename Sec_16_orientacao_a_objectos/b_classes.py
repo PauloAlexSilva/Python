6c778af20eb3951ee55619088df8b34c08b287c1
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-21 23:07:19 +0000

    Classes

--- Sec_16_orientacao_a_objectos/b_classes.py
+++ Sec_16_orientacao_a_objectos/b_classes.py
@@ -3,5 +3,13 @@ POO - Classes
 
 Em POO, Classes são modelos dos objetos do mundo real sendo representados computacionalmente.
 
+Sistema para automatizar o controlo das lampadas
+
 
 """
+
+idade = 32
+
+preco = 232323.32
+
+nome = 'Paulo'
