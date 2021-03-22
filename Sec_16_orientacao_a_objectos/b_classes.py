7cfff4da2854d59d68bfc5fe15cada014e20bfbe
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-22 22:31:55 +0000

    Classes

--- Sec_16_orientacao_a_objectos/b_classes.py
+++ Sec_16_orientacao_a_objectos/b_classes.py
@@ -6,10 +6,22 @@ Em POO, Classes são modelos dos objetos do mundo real sendo representados compu
 Sistema para automatizar o controlo das lampadas
 
 
+Classes podem conter:
+    - Atributos -> Representam as caracteristicas do objeto. Ou seja, pelos atributos conseguimos
+    representar computacionalmente os estados de um objeto. No caso da lampada, possivelmente
+    iriamos querer saber se a lampada é 110 ou 220 volts, de que cor é, etc
+
+    - Métodos (funções) -> Representam os comportamentos do objeto. Ou seja, as ações que este objeto
+    pode realizar no seu sistema. No caso da lâmpada, por exemplo, um coportamento comum que muito
+    provavelmente iríamos querer representar no nosso sistema  é o de ligar e desligar a mesma.
+
 """
 
-idade = 32
 
-preco = 232323.32
+class lampada:
+    pass
 
-nome = 'Paulo'
+
+lamp = lampada
+
+print(type(lampada))
