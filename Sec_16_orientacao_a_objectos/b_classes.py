cb05ae80a662d3e364277d1ce9c45c3e087a4ef2
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-22 22:45:13 +0000

    Classes

--- Sec_16_orientacao_a_objectos/b_classes.py
+++ Sec_16_orientacao_a_objectos/b_classes.py
@@ -15,13 +15,32 @@ Classes podem conter:
     pode realizar no seu sistema. No caso da lâmpada, por exemplo, um coportamento comum que muito
     provavelmente iríamos querer representar no nosso sistema  é o de ligar e desligar a mesma.
 
+
+Em Python, para definir uma classe utilizamos a palavra reservada class.
+
+OBS: Utilizamos a palavra 'pass' em Python quando temos um bloco de código que ainda não está
+implementado.
+
+
+OBS: Quando criamos uma classe em Python utilizamos por convenção o nome da classe começando com
+letra maiúscula. Se o nome for composto, utiliza-se as iniciais de ambas as palavras em maiúsculo,
+todas juntas.
+
+Dica: EM computação não utilizamos: Acentuação, caracteres especiais, espaços ou similares
+para nomes de classes, atributos, métodos, arquivos, diretórios etc.
+
+
 """
 
 
-class lampada:
+class Lampada:
     pass
 
 
-lamp = lampada
+class ContaCorrente:
+    pass
 
-print(type(lampada))
+
+lamp = Lampada
+
+print(type(Lampada))
