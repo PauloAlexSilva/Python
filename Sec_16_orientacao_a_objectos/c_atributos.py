b7a9b986d977fa554ec64ad70d081c870eedda0b
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-24 21:54:21 +0000

    Atrinutos

---
+++ Sec_16_orientacao_a_objectos/c_atributos.py
@@ -0,0 +1,13 @@
+"""
+POO - Atributos
+
+Atributos -> Representam as características do objeto. Ou seja, pelos atributos conseguimos
+representar computacionalmente os estados de um objeto.
+
+Em Python, dividimos os atributos em 3 grupos:
+    - Atributos de instância;
+    - Atributos de classe;
+    - Atributos Dinâmico.
+
+
+"""
