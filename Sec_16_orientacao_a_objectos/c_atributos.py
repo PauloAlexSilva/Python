583dba0c252232d2873528a5f1f3194468de12e4
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-03-24 22:35:11 +0000

    Atrinutos

--- Sec_16_orientacao_a_objectos/c_atributos.py
+++ Sec_16_orientacao_a_objectos/c_atributos.py
@@ -9,5 +9,62 @@ Em Python, dividimos os atributos em 3 grupos:
     - Atributos de classe;
     - Atributos Dinâmico.
 
+# Atributo de Instância: São atributos declarados dentro do método construtor.
+
+#OBS: Método construtor: É um método especial utilizado para a construção do objeto.
+
+
+# Em Java, uma classe Lampada, incluindo atributos ficaria mais ou menos:
+
+public class Lampada(){
+    private int voltagem;
+    private String cor;
+    private Boolean ligada = false;
+
+    public Lampada(int voltagem, String cor){
+        this.voltagem = voltagem;
+        this.cor = cor;
+    }
+
+    public int getVoltagem(){
+        return this.voltagem;
+    }
+
+}
 
 """
+
+
+class Lampada:
+    # Método construtor
+    def __init__(self, voltagem, cor):
+        # atributos
+        self.voltagem = voltagem
+        self.cor = cor
+        self.ligada = False
+
+
+class ContaCorrente:
+
+    def __init__(self, numero, limite, saldo):
+        self.numero = numero
+        self.limite = limite
+        self.saldo = saldo
+
+
+class Produto:
+
+    def __init__(self, nome, descricao, valor):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+
+
+class Utilizador:
+
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+# Atributos Públicos e Atributos Privados
