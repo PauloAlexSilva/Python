431c4ceca4a2fb6d992d845d35ae94be04d908ff
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-04-05 23:14:46 +0000

    Abstraction and Encapsulation

--- Sec_16_orientacao_a_objectos/f_abstracao_encapsulamento.py
+++ Sec_16_orientacao_a_objectos/f_abstracao_encapsulamento.py
@@ -1,4 +1,22 @@
 """
 POO - Abstração e Encapsulamento
 
+
 """
+
+class Utilizador:
+
+    def __init__(self, nome, sobrenome, email, password):
+        self.__nome = nome
+        self.__sobrenome = sobrenome
+        self.__email = email
+        self.__password = password
+
+
+cliente1 = Cliente('Paulo', '33223232')
+
+cc = ContaCorrente('5000', '10000', cliente1)
+
+cc.mostra_cliente()
+cliente1.diz()
+cc._ContaCorrente__cliente.diz()
