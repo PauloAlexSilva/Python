ff5a41937945ed1d3ad69bfb1f6c8c315beab348
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-04-06 22:18:30 +0000

    Abstraction and Encapsulation

--- Sec_16_orientacao_a_objectos/f_abstracao_encapsulamento.py
+++ Sec_16_orientacao_a_objectos/f_abstracao_encapsulamento.py
@@ -1,22 +1,33 @@
 """
 POO - Abstração e Encapsulamento
 
+O grande objetivo da POO é encapsular o código dentro de um grupo lógico e hierárquico utilizando
+classes.
+
+Encapsular -> Cápsula
+
+# Relembrar Atributos/Métodos privados em Python
+
+Temos uma classe chamada Pessoa, que contem
+um atributo privado chamado __nome e um método privado chamado __falar()
+
+Esses elementos privados só devem ser acessados dentro da classe.
+Mas em Pyhton não é bloqueado este acesso fora da classe. Com Python acontece um fenómeno chamado
+Name Mangling, que faz uma alteração na forma de se acessar os elementos privados, confrome:
+
+_Classe__elemento
+
+
+Exemplo - Acessar elementos privados fora da classe:
+
+instancia._Pessoa__nome
+
+instancia._Pessoa__falar()
+
+
+# Abstração em POO, é o ato de expor apenas dados relevantes de uma classe, escondendo atributos
+e métodos privados do utilizador.
+
 
 """
 
-class Utilizador:
-
-    def __init__(self, nome, sobrenome, email, password):
-        self.__nome = nome
-        self.__sobrenome = sobrenome
-        self.__email = email
-        self.__password = password
-
-
-cliente1 = Cliente('Paulo', '33223232')
-
-cc = ContaCorrente('5000', '10000', cliente1)
-
-cc.mostra_cliente()
-cliente1.diz()
-cc._ContaCorrente__cliente.diz()
