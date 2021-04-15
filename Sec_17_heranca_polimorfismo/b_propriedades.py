5c21c4d4f9c6a66aa9e936f7f4df849cf05ca618
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-04-15 23:22:26 +0000

    Properties

--- Sec_17_heranca_polimorfismo/b_propriedades.py
+++ Sec_17_heranca_polimorfismo/b_propriedades.py
@@ -7,3 +7,14 @@
 class Conta:
     contador = 0
 
+    def __init__(self, titular, saldo, limite):
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        Conta.contador += 1
+
+    def extrato(self):
+        return f'Saldo de {self.__saldo} do Cliente {self.__titular}'
+
+    def depositar(self, valor):
+        self.__saldo += valor
