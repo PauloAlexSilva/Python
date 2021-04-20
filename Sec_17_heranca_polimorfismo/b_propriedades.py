223f20597c6ab1926831c1cbc16dfb365f4dc3e3
Paulo Silva <pauloalexandresilva7689@gmail.com>
2021-04-20 22:46:53 +0000

    Properties

--- Sec_17_heranca_polimorfismo/b_propriedades.py
+++ Sec_17_heranca_polimorfismo/b_propriedades.py
@@ -8,6 +8,7 @@ class Conta:
     contador = 0
 
     def __init__(self, titular, saldo, limite):
+        self.__numero = Conta.contador
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
@@ -26,11 +27,26 @@ class Conta:
         self.__saldo -= valor
         destino.__saldo += valor
 
+    def get_numero(self):
+        return self.__numero
+
+    def
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
 
 conta1 = Conta('Paulo', 3000, 5000)
 conta2 = Conta('Ana', 2000, 4000)
 
-print(conta1. extrato())
+print(conta1.extrato())
 print(conta2.extrato())
 
-soma = conta1
+soma = conta1.get_saldo() + conta2.get_saldo()
+print(f'A soma do saldo das contas é {soma}')
